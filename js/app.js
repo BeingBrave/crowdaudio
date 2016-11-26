e80132a21862dacd5048c58b25052f58f94f628f
ryan <ryantwr@gmail.com>
2016-11-26 12:54:16 +0000

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,7 @@
+import '../css/app.scss';
+import $ from "jquery";
+
+$(function() {
+
+  console.log("Hello");
+});
---
+++ server.js
@@ -0,0 +1,28 @@
+"use strict";
+const request = require('request')
+const webpackDevServer = require('koa-webpack-dev');
+const app = require('koa')()
+const router = require('koa-router')()
+const send = require('koa-send')
+const json = require('koa-json')
+
+const port = process.env.PORT || 3000
+
+router.get('/', function *(){
+  yield send(this, 'index.html', { root: __dirname + '/public' })
+})
+
+app.use(webpackDevServer({
+    config: './webpack.dev.config.js'
+}));
+
+app.use(function *(next){
+  var start = new Date;
+  yield next;
+  var ms = new Date - start;
+	var timestamp = start.toISOString().replace(/T/, ' ').replace(/\..+/, '');
+  console.log('%s [%s]: %s %s - %sms', this.request.ip, timestamp, this.method, this.url, ms);
+}).use(json())
+	.use(router.routes())
+	.use(router.allowedMethods());
+app.listen(port)

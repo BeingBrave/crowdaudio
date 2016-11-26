f5d9f7b3b569a3e11c0d02c1ef405aa3ca6139bc
ryanwr <ryantwr@gmail.com>
2016-11-26 20:23:19 +0000

    Add network

--- js/network.js
+++ js/network.js
@@ -1,5 +1,21 @@
 import io from 'socket.io-client';
 
-$(function() {
-  var socket = io({id: 4});
-})
+class Network {
+  constructor(url) {
+    this.clientId = Math.floor(Math.random() * 100);
+    this.socket = io({
+      id: this.clientId
+    });
+  }
+
+  updateNode(node) {
+    // update node with id, x and y to server
+  }
+
+  updateDistance(sourceId, callback) {
+    // when this id's distance to source changes callback
+  }
+
+}
+
+export { Network as default}
